d052ae87ead432710e48c18f671783012f14a7a0
hackmajor <hackmajor@gmail.com>
2016-03-16 08:13:09 +0000

    Added basic tracker without style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315232509) do
+ActiveRecord::Schema.define(version: 20160316073148) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
