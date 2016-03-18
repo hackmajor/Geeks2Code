484ebace67677dede4e8b120c4cc757b8cd113fb
Matthew Smith <web.matthew.it@gmail.com>
2016-03-18 12:53:01 -0400

    removed .json action from tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,62 +3,46 @@ class TasksController < ApplicationController
 
   before_action :check_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
-
-    #@tasks = Task.paginate(page: params[:page], per_page: 10)
-
     @tasks = current_user.tasks.paginate(page: params[:page], per_page: 10)
-
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
+
   def show
   end
 
-  # GET /tasks/new
+
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
+
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
+
   def create
     @task = Task.new(task_params)
     @task.user = current_user
     respond_to do |format|
       if @task.save
         format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     @task.destroy
     respond_to do |format|
