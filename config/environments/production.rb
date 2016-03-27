0a4292c78802d5f324723916ff045c472a9e47e5
Matthew Smith <web.matthew.it@gmail.com>
2016-03-26 23:16:01 -0400

    setting gmail for sendign emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,13 +5,21 @@ Rails.application.configure do
   config.cache_classes = true
   config.time_zone = "Pacific Time (US & Canada)"
 
-  config.action_mailer.default_url_options = {:host => 'https://my-task-tracker.herokuapp.com'}
+  config.action_mailer.default_url_options = { :host => 'https://my-task-tracker.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    :address => "127.0.0.1",
-    :port    => 25,
-    :domain  => 'https://my-task-tracker.herokuapp.com'
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "https://my-task-tracker.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
+
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
