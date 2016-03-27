eb97b7d466c60cfd561159db2a9a7818624a0a11
Matthew Smith <web.matthew.it@gmail.com>
2016-03-27 00:47:35 -0400

    fixing env production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,16 +11,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "https://my-task-tracker.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV["GMAIL_USERNAME"],
+   :password             => ENV["GMAIL_PASSWORD"],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
   }
-
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
