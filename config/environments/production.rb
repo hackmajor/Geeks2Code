8ad7f9add17b5e974c0fa212ead8c6a81186937b
Matthew Smith <web.matthew.it@gmail.com>
2016-03-30 14:04:50 -0400

    update general styles, added new credential and config for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,12 @@ Rails.application.configure do
   }
 
   config.paperclip_defaults = {
-      :storage => :s3,
+      :storage => :fog,
       :s3_credentials => {
-        :bucket => ENV['S3_BUCKET_NAME'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        :provider => 'AWS',
+        :bucket => ENV['S3_BUCKET'],
+        :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY']
       }
     }
 
